6bb7cc188418debe20342cb68d6088c798b469d8
DAHAI LI <blizzzrdda@gmail.com>
2024-10-24 17:04:36 -0400

    Upgrade to unity 6

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -13,27 +13,27 @@ namespace Map
 
         private void Start()
         {
-            if (PlayerPrefs.HasKey("Map"))
-            {
-                string mapJson = PlayerPrefs.GetString("Map");
-                Map map = JsonConvert.DeserializeObject<Map>(mapJson);
-                // using this instead of .Contains()
-                if (map.path.Any(p => p.Equals(map.GetBossNode().point)))
-                {
-                    // payer has already reached the boss, generate a new map
-                    GenerateNewMap();
-                }
-                else
-                {
-                    CurrentMap = map;
-                    // player has not reached the boss yet, load the current map
-                    view.ShowMap(map);
-                }
-            }
-            else
-            {
+            // if (PlayerPrefs.HasKey("Map"))
+            // {
+            //     string mapJson = PlayerPrefs.GetString("Map");
+            //     Map map = JsonConvert.DeserializeObject<Map>(mapJson);
+            //     // using this instead of .Contains()
+            //     if (map.path.Any(p => p.Equals(map.GetBossNode().point)))
+            //     {
+            //         // payer has already reached the boss, generate a new map
+            //         GenerateNewMap();
+            //     }
+            //     else
+            //     {
+            //         CurrentMap = map;
+            //         // player has not reached the boss yet, load the current map
+            //         view.ShowMap(map);
+            //     }
+            // }
+            // else
+            // {
                 GenerateNewMap();
-            }
+            // }
         }
 
         public void GenerateNewMap()
